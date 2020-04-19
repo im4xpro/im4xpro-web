10cdf9930d961f0441f251865f663f787981ce0f
Maximilian <m.kaiser.03@t-online.de>
2020-04-19 22:39:59 +0200

    skill panel & website title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import lax from 'lax.js';
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,8 +12,14 @@ export class AppComponent {
   title = 'im4xpro-web';
   titleword: string = "design";
   element : any;
-  activeSkill : any;
+  activeSkill : string;
+
+constructur(){ }
+
+
 ngOnInit(){
+
+  // Lax
   window.onload = function() {
 	lax.setup() // init
 
@@ -24,8 +33,22 @@ ngOnInit(){
 
 }
 
-openSkill(){
-  
+ openSkill(skill){
+   this.skillreset();
+  this.element = document.getElementById("skillstext");
+  this.element.style.display = "block";
+  this.activeSkill = skill;
+  switch(skill){
+    case "HTML": this.element = document.getElementById("text-html");
+    break;
+    case "CSS": this.element = document.getElementById("text-css");
+    break;
+    case "JAVASCRIPT": this.element = document.getElementById("text-js");
+    break;
+    case "ANGULAR": this.element = document.getElementById("text-angular");
+    break;
+  }
+  this.element.style.display = "block";
 
 }
 
@@ -33,5 +56,16 @@ closeSkill(){
   this.element = document.getElementById("skillstext");
   this.element.style.display = "none";
   this.activeSkill = "";
+}
+
+skillreset(){
+  this.element = document.getElementById("text-html");
+  this.element.style.display = "none";
+  this.element = document.getElementById("text-css");
+  this.element.style.display = "none";
+  this.element = document.getElementById("text-js");
+  this.element.style.display = "none";
+  this.element = document.getElementById("text-angular");
+  this.element.style.display = "none";
 }
 }
