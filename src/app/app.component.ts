47bc0ce18cfb5432e75331414663de0cb0a8caa9
Maximilian <m.kaiser.03@t-online.de>
2020-04-20 11:38:51 +0200

    skill-description update
    shows the description to each skill

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,13 +41,13 @@ this.changeText();
   this.element.style.display = "block";
   this.activeSkill = skill;
   switch(skill){
-    case "HTML": this.element = document.getElementById("text-html");
+    case "HTML": this.element = document.getElementById("text-html"); this.element.innerHTML = document.getElementById("htmlsrc").innerHTML;
     break;
-    case "CSS": this.element = document.getElementById("text-css");
+    case "CSS": this.element = document.getElementById("text-css"); this.element.innerHTML = document.getElementById("csssrc").innerHTML;
     break;
-    case "JAVASCRIPT": this.element = document.getElementById("text-js");
+    case "JAVASCRIPT": this.element = document.getElementById("text-js"); this.element.innerHTML = document.getElementById("jssrc").innerHTML;
     break;
-    case "ANGULAR": this.element = document.getElementById("text-angular");
+    case "ANGULAR": this.element = document.getElementById("text-angular"); this.element.innerHTML = document.getElementById("angularsrc").innerHTML;
     break;
   }
   this.element.style.display = "block";
@@ -63,12 +63,16 @@ closeSkill(){
 skillreset(){
   this.element = document.getElementById("text-html");
   this.element.style.display = "none";
+  this.element.innerHTML = "";
   this.element = document.getElementById("text-css");
   this.element.style.display = "none";
+  this.element.innerHTML = "";
   this.element = document.getElementById("text-js");
   this.element.style.display = "none";
+  this.element.innerHTML = "";
   this.element = document.getElementById("text-angular");
   this.element.style.display = "none";
+  this.element.innerHTML = "";
 }
 
 
